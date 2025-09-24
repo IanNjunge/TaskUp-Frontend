427a7d452f5615e0144205510c542740fc38ab55
IanNjunge <ian.njunge@student.moringaschool.com>
2025-09-24 13:04:45 +0300

    Modifies main.jsx and App.jsx

--- TaskUp/src/main.jsx
+++ TaskUp/src/main.jsx
@@ -6,8 +6,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
